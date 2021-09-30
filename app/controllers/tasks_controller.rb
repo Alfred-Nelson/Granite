86e0420e5d7c33cf5a0f11e6dd2f5c6dfbb38fda
Alfred Nelson <alfrednelson@Alfreds-MacBook-Pro.local>
2021-09-30 19:29:12 +0530

    Implemented create action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,4 +5,20 @@ class TasksController < ApplicationController
     @tasks = Task.all
     render status: :ok, json: { tasks: @tasks }
   end
+
+  def create
+    task = Task.create(task_params)
+    if task.save
+      render status: :ok, json: { notice: t("success") }
+    else
+      errors = task.error.full_messages.to_sentance
+      render status: :unprocessable_entity, json: { error: errors }
+    end
+  end
+
+  private
+
+    def task_params
+      params.require(:task).permit(:title)
+    end
 end
